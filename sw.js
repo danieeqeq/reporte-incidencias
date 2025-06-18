7c9c21c71cb5eabc9b6190d20aa246b71a2cc440
danieeqeq <142830435+danieeqeq@users.noreply.github.com>
2025-06-17 19:10:21 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
 
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('reporte-cache').then(function (cache) {
+      return cache.addAll([
+        './',
+        './reportes.html',
+        './reportes.css',
+        './reportes.js',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
